6356c7f0041960ff9d618d1e4912bec428953a85
Ana Abad Lorenzo <anaabadlorenzo24@gmail.com>
2024-01-20 18:08:08 +0100

    Add deploy to travis

---
+++ rest/src/main/java/es/uvigo/esei/dgss/teama/microstories/MyApplication.java
@@ -0,0 +1,9 @@
+package es.uvigo.esei.dgss.teama.microstories;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class MyApplication extends Application {
+    // Application configuration, if needed
+}
